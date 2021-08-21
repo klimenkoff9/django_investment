11c89bd720ff6fc565702126eb08b4fd3c256265
klimenkoff9 <dennis.klimenkov@washpost.com>
2021-08-21 15:22:04 -0400

    create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Store(models.Model):
+    key = models.CharField(max_length=100, unique=True)
+    passphrase = models.CharField(max_length=100, unique=True)
+    secret = models.CharField(max_length=100, unique=True)
+
+class Order(models.Model):
+    product_id = models.CharField(max_length=10)
+    funds = models.FloatField()
+    side = models.CharField(max_length=10)
+
