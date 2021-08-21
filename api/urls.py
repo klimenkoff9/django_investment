75aaa6c976230aacc01d3ce00bcf6f8efbae6582
klimenkoff9 <dennis.klimenkov@washpost.com>
2021-08-21 15:22:46 -0400

    create urls

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import main
+from .views import StoreView, CreateStoreView, PlaceOrder
 
 urlpatterns = [
-    path('buy', main),
+    path('login', StoreView.as_view()),
+    path('create', CreateStoreView.as_view()),
+    path('get', PlaceOrder.as_view())
 ]
