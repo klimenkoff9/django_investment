1b2da1ae2da004a089c4040b771bd407c353cfcc
klimenkoff9 <dennis.klimenkov@washpost.com>
2021-08-28 10:59:56 -0400

    update urls

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import StoreView, CreateStoreView, PlaceOrder
 
 urlpatterns = [
-    path('login', StoreView.as_view()),
-    path('create', CreateStoreView.as_view()),
-    path('get', PlaceOrder.as_view())
+    path('view', StoreView.as_view()),
+    path('add-key', CreateStoreView.as_view()),
+    path('place-order', PlaceOrder.as_view())
 ]
